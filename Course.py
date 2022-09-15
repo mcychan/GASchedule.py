4c4b33a4d16794b085888c922bde91c0f37ac536
Miller Cy Chan <miller.chan@gmail.com>
2022-09-15 16:48:54 +0800

    Delete Course.py

--- Course.py
+++
@@ -1,8 +0,0 @@
-# Stores data about course
-class Course:
-    # Initializes course
-    def __init__(self, id, name):
-        # Returns course ID
-        self.Id = id
-        # Returns course name
-        self.Name = name
