eef76384f7d3894a90e6fdb35d167a446853e378
Miller Cy Chan <miller.chan@gmail.com>
2022-09-21 11:52:20 +0800

    Add files via upload

--- model/Reservation.py
+++ model/Reservation.py
@@ -10,17 +10,16 @@ class Reservation:
         self.Day = day
         self.Time = time
         self.Room = room
-        self._index = self.Day * Reservation.NR * Constant.DAY_HOURS + self.Room * Constant.DAY_HOURS + self.Time
 
 
     def __hash__(self) -> int:
-        return self._index
+        return self.Day * Reservation.NR * Constant.DAY_HOURS + self.Room * Constant.DAY_HOURS + self.Time
 
 
     def __eq__(self, other):
         if not isinstance(other, self.__class__):
             return False
-        return self._index == other._index
+        return hash(self) == hash(other)
             
     def __ne__(self, other):
         return not self.__eq__(other)
