ed0c6503e2f942f0651ddabd663f66f2681857d1
Miller Cy Chan <miller.chan@gmail.com>
2022-09-22 22:02:19 +0800

    Update Hgasso.py

--- algorithm/Hgasso.py
+++ algorithm/Hgasso.py
@@ -63,10 +63,14 @@ class Hgasso(NsgaII):
 
     def updateVelocities(self, population):
         populationSize = len(population)
-        dim = np.shape(self._velocity)
-        self._velocity = np.random.random(dim) * np.log10(np.random.uniform(7.0, 14.0, dim)) * self._velocity + \
-        np.log10(np.random.uniform(7.0, 14.0, dim)) * np.log10(np.random.uniform(35.5, 38.5, dim)) * (self._sBest - self._current_position) + \
-        np.log10(np.random.uniform(7.0, 14.0, dim)) * np.log10(np.random.uniform(35.5, 38.5, dim)) * (self._sgBest - self._current_position)
+        for i in range(populationSize):
+            dim = len(self._velocity[i])
+            for j in range(dim):
+                self._velocity[i][j] = np.random.random() * np.log10(np.random.uniform(7.0, 14.0)) * self._velocity[i][j] + \
+                                 np.log10(np.random.uniform(7.0, 14.0)) * np.log10(np.random.uniform(35.5, 38.5)) * (
+                                             self._sBest[i][j] - self._current_position[i][j]) + \
+                                 np.log10(np.random.uniform(7.0, 14.0)) * np.log10(np.random.uniform(35.5, 38.5)) * (
+                                             self._sgBest[j] - self._current_position[i][j])
 
         self._current_position += self._velocity
 
