7dd7dd5caaa7f8a42df0625270f6bbc42486ffcb
Miller Cy Chan <miller.chan@gmail.com>
2022-09-22 13:53:12 +0800

    Add files via upload

--- ConsoleApp.py
+++ ConsoleApp.py
@@ -16,35 +16,35 @@ from HtmlOutput import HtmlOutput
 
 
 def main(file_name):
-    try:
-        start_time = int(round(time.time() * 1000))
+    start_time = int(round(time.time() * 1000))
 
-        configuration = Configuration()
-        target_file = str(pathlib.Path().absolute()) + file_name
-        configuration.parseFile(target_file)
+    configuration = Configuration()
+    target_file = str(pathlib.Path().absolute()) + file_name
+    configuration.parseFile(target_file)
 
-        # alg = NsgaII(configuration)
-        alg = Hgasso(configuration)
-        print("GaSchedule Version 1.2.1 . Making a Class Schedule Using", alg, ".\n")
-        print("Copyright (C) 2022 Miller Cy Chan.\n")
-        alg.run()
-        html_result = HtmlOutput.getResult(alg.result)
+    # alg = NsgaII(configuration)
+    alg = Hgasso(configuration)
+    print("GaSchedule Version 1.2.1 . Making a Class Schedule Using", alg, ".\n")
+    print("Copyright (C) 2022 Miller Cy Chan.\n")
+    alg.run()
+    html_result = HtmlOutput.getResult(alg.result)
 
-        temp_file_path = tempfile.gettempdir() + file_name.replace(".json", ".htm")
-        writer = codecs.open(temp_file_path, "w", "utf-8")
-        writer.write(html_result)
-        writer.close()
+    temp_file_path = tempfile.gettempdir() + file_name.replace(".json", ".htm")
+    writer = codecs.open(temp_file_path, "w", "utf-8")
+    writer.write(html_result)
+    writer.close()
 
-        seconds = (int(round(time.time() * 1000)) - start_time) / 1000.0
-        print("\nCompleted in {} secs.\n".format(seconds))
-        os.startfile(temp_file_path)
-    except:
-        traceback.print_exc()
+    seconds = (int(round(time.time() * 1000)) - start_time) / 1000.0
+    print("\nCompleted in {} secs.\n".format(seconds))
+    os.startfile(temp_file_path)
 
 
 if __name__ == "__main__":
-    try:
+    file_name = "/GaSchedule.json"
+    if len(sys.argv) > 1:
         file_name = sys.argv[1]
-    except IndexError:
-        file_name = "/GaSchedule.json"
-    main(file_name)
+
+    try:
+        main(file_name)
+    except:
+        traceback.print_exc()
