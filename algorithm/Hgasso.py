6cdc09c8a601a92727c31e442066efa97604d926
Miller Cy Chan <miller.chan@gmail.com>
2022-09-23 13:49:31 +0800

    Add files via upload

--- algorithm/Hgasso.py
+++ algorithm/Hgasso.py
@@ -28,8 +28,6 @@ class Hgasso(NsgaII):
             if fitness < self._sBestScore[i]:
                 population[i].updatePositions(self._current_position[i])
                 fitness = population[i].fitness
-            else:
-                population[i].extractPositions(self._current_position[i])
 
             if fitness > self._sBestScore[i]:
                 self._sBestScore[i] = fitness
@@ -65,9 +63,12 @@ class Hgasso(NsgaII):
 
     def updateVelocities(self, population):
         dim = self._velocity.shape
-        self._velocity = np.random.random(dim) * np.log10(np.random.uniform(7.0, 14.0, dim)) * self._velocity[:] + \
-        np.log10(np.random.uniform(7.0, 14.0, dim)) * np.log10(np.random.uniform(35.5, 38.5, dim)) * (self._sBest - self._current_position[:]) + \
-        np.log10(np.random.uniform(7.0, 14.0, dim)) * np.log10(np.random.uniform(35.5, 38.5, dim)) * (self._sgBest - self._current_position[:])
+        self._velocity = np.random.random(dim) * np.log10(np.random.uniform(7.0, 14.0, dim)) * self._velocity + \
+        np.log10(np.random.uniform(7.0, 14.0, dim)) * np.log10(np.random.uniform(35.5, 38.5, dim)) * (
+        self._sBest - self._current_position) + \
+        np.log10(np.random.uniform(7.0, 14.0, dim)) * np.log10(np.random.uniform(35.5, 38.5, dim)) * (
+        self._sgBest - self._current_position)
+
         self._current_position += self._velocity
 
     def __str__(self):
