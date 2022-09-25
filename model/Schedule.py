7ca43f40abf6ad1b2f697d0a9ac846a5ee4d82d9
Miller Cy Chan <miller.chan@gmail.com>
2022-09-25 08:40:43 +0800

    Add files via upload

--- model/Schedule.py
+++ model/Schedule.py
@@ -33,7 +33,7 @@ class Schedule:
         if not setup_only:
             self._configuration = c.configuration
             # copy code
-            self._slots, self._classes = c.slots[:], dict(c.classes)
+            self._slots, self._classes = c.slots[:], OrderedDict(c.classes)
 
             # copy flags of class requirements
             self._criteria = c.criteria[:]
